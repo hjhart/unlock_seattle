39e34dfb0a299e60b817a0401d6eef68b2eb2302
James Hart <hjhart@gmail.com>
2013-01-11 22:31:07 -0800

    Rails checkin

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UnlockSeattle::Application.config.secret_token = 'f8f400f77147d36b7aabf559ed214d6879c8d7af5b8aed78fdc69f6c73680c61d5f6eba0fc3536e894ca921ef7fb5d5e77afc67d853ce46e22bbb30dd7f3ae36'
